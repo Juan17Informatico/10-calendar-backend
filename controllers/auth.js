1f8f8a0857ef1cd98a45e3f912caf26de2d88c6a
JuanCM <juancampuzano2356@gmail.com>
2024-08-22 17:05:16 -0500

    First commit

---
+++ controllers/auth.js
@@ -0,0 +1,35 @@
+const { response } = require('express');
+
+const crearUsuario = (req, res = response ) => {
+
+    res.json({
+        ok: true,
+        msg: 'Register',
+    });
+
+}
+
+const loginUsuario = (req, res) => {
+
+    res.json({
+        ok: true,
+        msg: 'login',
+    });
+
+}
+
+const revalidarToken = (req, res) => {
+
+    res.json({
+        ok: true,
+        msg: 'renew',
+    });
+
+}
+
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken,
+}
