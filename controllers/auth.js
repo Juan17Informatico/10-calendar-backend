0fe2592b162e3ca4c32afcebf7a46269f471cb91
JuanCM <juancampuzano2356@gmail.com>
2024-08-29 17:11:29 -0500

    Se finaliza la clase 382

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,31 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario'); 
 
-const crearUsuario = (req, res = response ) => {
+const crearUsuario = async(req, res = response ) => {
 
-    const { name, email, password } = req.body;
+    // const { name, email, password } = req.body;
 
-    res.json({
-        ok: true,
-        msg: 'Registro',
-        name,
-        email,
-        password
-    });
+    try {
+        
+        const usuario = new Usuario( req.body );  
+    
+        await usuario.save();
+    
+        res.status(201).json({
+            ok: true,
+            msg: 'Registro'
+        });
+
+    } catch (error) {
+        console.log(error);
+    
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        })
+
+    }
+
 
 }
 
