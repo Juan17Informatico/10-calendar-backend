3e97f80acf291ea65abfc510501e737a55684913
JuanCM <juancampuzano2356@gmail.com>
2024-09-06 16:51:54 -0500

    Fin de la sección 23

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express'); 
 const { dbConnection } = require('./database/config');
+const cors = require('cors'); 
 require('dotenv').config();
 
 
@@ -8,6 +9,9 @@ const app = express();
 
 // Base de datos
 dbConnection();
+
+//Cors
+app.use(cors()); 
 
 // Directorio público
 app.use( express.static('public') );
